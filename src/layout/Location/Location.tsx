4a029f774bdf9d86fdfd0ad17384607623a7c00e
guleegulee <progulee@naver.com>
2025-10-14 14:27:07 +0900

    🅿️ 주차장 안내 이미지 추가

--- src/layout/Location/Location.tsx
+++ src/layout/Location/Location.tsx
@@ -4,6 +4,7 @@ import Address from './Address.tsx';
 import Map from './Map.tsx';
 import MapButtons from './MapButtons.tsx';
 import { Caption, PointTitle } from '@/components/Text.tsx';
+import parkingImage from '@/assets/images/park.jpg'; // ✅ 이미지 경로 추가
 
 const Location = () => {
   const { mapInfo } = data;
@@ -12,6 +13,12 @@ const Location = () => {
       <PointTitle>{mapInfo.address1}</PointTitle>
       <Caption textAlign={'center'}>{mapInfo.address2}</Caption>
       <Map />
+      {/* ✅ 이미지 삽입 */}
+      <img
+        src={parkingImage}
+        alt="주차장 안내"
+        style={{ width: '100%', marginTop: '12px', borderRadius: '8px' }}
+      />
       <MapButtons />
       <Address />
     </LocationWrapper>
